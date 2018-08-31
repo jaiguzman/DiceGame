eafc646470772a5b0220158da46c878d7809c434
Jaime G <jaime.guzman92@gmail.com>
2018-08-31 12:38:59 -0700

    fixed JavaScript file

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ GAME RULES:
 
 */
 
-/*
-3 extra Challenges
-channge the game to follow these rules:
-1. A player looses his Entire Score when he rolls two 6 in a row. After that, its; the next
-  player's turn. (hint: Always save the pervious dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can
-  change the predefined score of 100. (Hint: you can read that value with the .value property
-  in JavaScript. This is a good oppotunity to use google to figure this out)
-3. Add another dice to the game, so that there are two dices now. The player looses his
-  current score when one of them is a 1. (Hint: you will need CSS to position the second
-  dice, so take a look at the CSS code for the first one.)
-*/
 
 var scores, roundScore, activePlayer, gamePlaying;
 
